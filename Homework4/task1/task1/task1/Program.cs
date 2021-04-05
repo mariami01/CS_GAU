c81e1a715b0fc0a8c6b46329ffa1de1ed22c90e1
mariami01 <zenaishvilimariam@gmail.com>
2021-04-05 21:10:13 +0400

    tasks done

---
+++ Homework4/task1/task1/task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Rectangle rec = new Rectangle(8, 5);
+            Triangle tr = new Triangle(3,4,5);
+            Console.WriteLine("The perimeter of the rectangle is: {0}\n", rec.GetPerim());
+            Console.WriteLine("The perimeter of the triangle is: {0}\n", tr.GetPerim());
+        }
+    }
+}
---
+++ Homework4/task1/task1/task1/figure.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace task1
+{
+    public abstract class Shape
+    {
+        public abstract double GetPerim();
+        
+    }
+
+    public class Rectangle : Shape
+    {
+        protected double length, width;
+        public Rectangle(double length, double width)
+        {
+            this.length = length;
+            this.width = width;
+        }
+        public override double GetPerim()
+        {
+            return (length + width) * 2;
+
+        }
+    }
+
+
+    public class Triangle : Shape
+    {
+        protected double side1, side2, side3;
+        public Triangle(double side1, double side2, double side3)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+            this.side3 = side3;
+        }
+        public override double GetPerim()
+        {
+            return side1 + side2 + side3;
+
+        }
+    }
+}
