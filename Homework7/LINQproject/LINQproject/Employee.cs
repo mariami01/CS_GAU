88c266e1feb9f388144f380cd057e7b719a29f93
mariami01 <57932258+mariami01@users.noreply.github.com>
2021-05-13 23:38:24 +0400

    Finished the homework 📚 😊 ⭐️

---
+++ Homework7/LINQproject/LINQproject/Employee.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace LINQproject
+{
+    class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public int ProjectId { get; set; }
+    } 
+}
