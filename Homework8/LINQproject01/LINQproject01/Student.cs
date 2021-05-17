ded8fc69a4415dd6c72d06072ffab1c1a99787c1
mariami01 <57932258+mariami01@users.noreply.github.com>
2021-05-17 18:43:33 +0400

    done ✔️

---
+++ Homework8/LINQproject01/LINQproject01/Student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LINQproject01
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Major { get; set; }
+    }
+}
