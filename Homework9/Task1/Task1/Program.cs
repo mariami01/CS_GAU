82c5b1ec0e4bc33967f4f1ad73895b59852ceeb3
mariami01 <57932258+mariami01@users.noreply.github.com>
2021-05-22 22:57:27 +0400

    finished ✅

---
+++ Homework9/Task1/Task1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // task 12.1.4
+
+            //string text = "Hello World!";
+            Console.WriteLine("Text:");
+
+            string text = Console.ReadLine();
+
+            string symbol = "!";
+
+            int numberOfTrues = Regex.Matches(text, symbol).Count;
+
+            Console.WriteLine( "The symbol '!' occures {0} times in a string.",numberOfTrues);
+
+        }
+    }
+}
---
+++ Homework9/Task2/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Text:");
+
+            string text = Console.ReadLine();
+
+            string replacedText = text.Replace("i", "s");
+
+            Console.WriteLine("Your Text is '{0}'. We replaced 'i' with 's'. Final text: {1}", text, replacedText);
+        }
+    }
+}
