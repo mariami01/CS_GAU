4ec159e9cb47f4100dbbaae5632d5f01544367fe
mariami01 <57932258+mariami01@users.noreply.github.com>
2021-05-23 00:35:50 +0400

    uno 😊 🎉

---
+++ Homework10/Task01/Task01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 14.1.1
+            //შექმენით 10-ელემენტიანი დინამიური მასივი სტრიქონების მასივის საფუძველზე.
+            //დინამიურ მასივს დაუმატეთ ორი სტრიქონი. ეკრანზე გამოიტანეთ დინამიურ მასივში
+            //ელემენტების რაოდენობა და თვით დინამიური მასივი.
+
+            string[] elements = new string[] { "Element1", "Element2", "Element3", "Element4", "Element5", "Element6", "Element7", "Element8", "Element9", "Element10" };
+
+            ArrayList arrList = new ArrayList(elements);
+            arrList.Add("randElement1");
+            arrList.Add("randElement2");
+
+            Console.WriteLine("\n-------The array elements-------");
+
+            foreach (var item in arrList)
+                Console.Write(item + ", ");
+
+            Console.WriteLine("\n\nNumber of elements in the array: {0}", arrList.Count);
+
+        }
+    }
+}
---
+++ Homework10/Task02/Task02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+
+namespace Task02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //14.1.2. შექმენით 10-ელემენტიანი დინამიური მასივი სტრიქონების მასივის საფუძველზე.
+            //დინამიური მასივიდან წაშალეთ სამი სტრიქონი. ეკრანზე გამოიტანეთ დინამიურ მასივში 
+            //ელემენტების რაოდენობა და თვით დინამიური მასივი.
+
+
+            string[] elements = new string[] { "myElement1", "myElement2", "myElement3", "myElement4", "myElement5", "myElement6", "myElement7", "myElement8", "myElement9", "myElement10" };
+
+            ArrayList arrList = new ArrayList(elements);
+
+            Console.WriteLine("-------The elements in ArrayList initially are-------");
+
+            foreach (var item in arrList)
+                Console.Write(item + ", ");
+
+            arrList.RemoveAt(4);
+            arrList.RemoveAt(7);
+
+            Console.WriteLine("\n\n-------The elements in ArrayList are-------");
+
+            foreach (var item in arrList)
+                Console.Write(item + ", ");
+
+            Console.WriteLine("\n\nNumber of elements in the array: {0}", arrList.Count);
+        }
+    }
+}
